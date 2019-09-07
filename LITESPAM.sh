c99029ac5190df125ceb7a60a9d3c3ff6e2360cf
masterz345 <54578444+masterz345@users.noreply.github.com>
2019-09-08 00:14:51 +0700

    Add files via upload

---
+++ LITESPAM.sh
@@ -0,0 +1,178 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
+
+toilet -f slant --gay "MasterZ"
+sleep 1
+echo "\033[33;1m tool untuk Penyerang By MasterZ"
+sleep 1
+echo "\033[36;1m★""\033[34;1mNetWork Operation Center""\033[36;1m★"
+sleep 1
+echo "\033[32;1m Menu Penyerang Yang Tersedia :"
+echo "\033[35;1m1.""\033[36;1mBukalapak"
+echo "\033[35;1m2.""\033[36;1mTelkomsel"
+echo "\033[35;1m3.""\033[36;1mGrab"
+echo "\033[35;1m4.""\033[36;1mTokopedia"
+echo "\033[35;1m5.""\033[36;1mCodaShop"
+echo "\033[35;1m6.""\033[36;1mSurveyon"
+echo "\033[35;1m7.""\033[36;1mCustom"
+echo "\033[31;1m0. Keluar"
+echo "\033[37;1m81.""\033[30;1mInformasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant "Bukalapak"
+echo "\033[31;1mJenis Spam:""\033[37;1mChat WhatsApp"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mNetWork Operation Center"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 1
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mNetWork Operation Center"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Grab"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mNetWork Operation Center"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono9 -F gay "Toked"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1m1 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mNetWork Operation Center"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 4.php
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f slant -F gay "CodaShop"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mNetWork Operation Center"
+echo
+echo
+echo "\033[33;1mCOPY LISENSI INI:" "\033[36;1mJDJ5JDEwJC9YMWRvNC5jcmNOSUw2dGdUaUt2d08ucWEvWURWSFNCTXI3U21wdkdsR1FMcVBSTW1oNUZH"
+sleep 1
+echo "\033[32;1mtunggu 5 Detik"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Surveyon "
+echo "\033[31;1mJenis Spam:""\033[37;1mEmail"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mphp"
+echo "\033[31;1mAuthor:""\033[37;1mNetWork Operation Center"
+sleep 5
+cd $HOME/LITESPAM/Tul/
+php 6.php
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f slant --gay "SMSID"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m5"
+echo "\033[31;1mPemograman:""\033[37;1mBerjalan Langsung Pada MasterZ"
+echo "\033[31;1mAuthor:""\033[37;1mNetWork Operation Center"
+echo
+echo
+echo "\033[33;1mInformasi Lebih Lanjut Kunjungi:""\033[36;1mhttps://github.com/masterz345/"
+sleep 6
+smsid boom -y
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "MasterZ"
+echo "\033[34;1mContact:""\033[37;1m https://instagram.com/masterz345"
+
+
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+
+echo "\033[33;1mKarya: MasterZ"
+sleep 1
+echo "\033[32;1mVersi: 2.3"
+sleep 1
+echo "\033[36;1mTeam: NetWork Operation Center"
+sleep 1
+echo "\033[31;1mSubscribe YouTube""\033[37;1m MasterZFF"
+echo "\033[35;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "MasterZ"
+echo "Mr.HC"
+echo "CyberTeamIndonesia"
+echo "NetWork Operation Center"
+sleep 1
+echo "And All Member" "NetWork Operation Center"
+echo "\033[30;1m tunggu Sayang.."
+sleep 8
+sh LITESPAM.sh
+fi
+
+
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
+
